aad78db4de06e502c62f116762fdac89fb318ab7
Ruben civeira <ruben.civeiraiglesias@plexus.es>
2023-11-26 21:03:28 +0100

    feat: webContext ejecutable

--- src/WebContext.php
+++ src/WebContext.php
@@ -1,7 +1,9 @@
 <?php
 namespace Civi\Micro;
 
+use Slim\Factory\AppFactory;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 class WebContext extends Context {
     private static $_filters = [];
@@ -10,7 +12,27 @@ class WebContext extends Context {
         self::$_filters[] = $filters;
     }
 
-    public function filter(Request $request): Request {
+    public function __construct(private readonly string $basePath) {
+    }
+
+    public function start(\Closure $routes) {
+        $app = AppFactory::create();
+        $app->add( function (Request $request, RequestHandler $handler) {
+            $uri = $request->getUri();
+            $path = $uri->getPath();
+
+            if (str_starts_with($path, $this->basePath)) {
+                $path = substr($path, strlen($this->basePath));
+                $uri = $uri->withPath($path);
+                $request = $request->withUri($uri);
+            }
+            return $handler->handle( $this->filter( $request ) );
+        });
+        $routes($app, $this->build());
+        $app->run();
+    }
+
+    private function filter(Request $request): Request {
         $container = $this->currentContainer();
         foreach(self::$_filters as $filters) {
             foreach($filters as $filter) {
